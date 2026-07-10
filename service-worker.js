190055a1435b875c0c4a819cbcfea10d1caaa64b
Marc Pioquinto <33591263+neomrc@users.noreply.github.com>
2026-07-10 21:00:34 +0800

    Refresh cache for keyboard stability fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="crossword-quest-public-v3";
+const CACHE="crossword-quest-public-v4";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
