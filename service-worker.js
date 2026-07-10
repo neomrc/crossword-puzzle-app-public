85b9dacbd2dda0b4adf5c51a1ae0971fa1452d7f
Marc Pioquinto <33591263+neomrc@users.noreply.github.com>
2026-07-10 21:52:05 +0800

    Refresh cache for responsive clue bar

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="crossword-quest-public-v14";
+const CACHE="crossword-quest-public-v15";
 const ASSETS=["./","./index.html","./styles.css","./native-enhancements.css","./puzzle-seed.js","./app.js","./app-enhancements.js","./manifest.webmanifest","./icons/icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
